e05dc0444452c93345be25346de4a61ea450af8a
Salvage <29021710+Saalvage@users.noreply.github.com>
2024-11-27 17:24:17 +0100

    Restore full functionality

--- CBRE.Editor/Settings/FileTypeRegistration.cs
+++ CBRE.Editor/Settings/FileTypeRegistration.cs
@@ -1,52 +1,38 @@
-﻿using Microsoft.Win32;
-using System;
-using System.Collections.Generic;
-using System.Reflection;
+﻿using System.Collections.Generic;
+using System.Linq;
 using NativeFileDialogNET;
 
-namespace CBRE.Editor.Settings
-{
-    public static class FileTypeRegistration
-    {
-        public const string ProgramId = "CBREEditor";
-        public const string ProgramIdVer = "1";
+namespace CBRE.Editor.Settings;
 
-        public static FileType[] GetSupportedExtensions()
-        {
-            return new[]
-            {
-                new FileType(".vmf", "Valve Map File", true, true),
-                new FileType(".rmf", "Worldcraft RMF", true, true),
-                new FileType(".map", "Quake MAP Format", true, true),
-                new FileType(".3dw", "Leadwerks 3D World Studio File", false, true),
+public static class FileTypeRegistration {
+    public static IReadOnlyList<FileType> SupportedExtensions { get; } = [
+        new("vmf", "Valve Map Format", true, true),
+        new("rmf", "Worldcraft RMF", true, true),
+        new("map", "Quake MAP Format", true, true),
+        new("3dw", "Leadwerks 3D World Studio File", false, true),
 
-                new FileType(".rmx", "Worldcraft RMF (Hammer Backup)", false, true),
-                new FileType(".max", "Quake MAP Format (Hammer Backup)", false, true),
-                new FileType(".vmx", "Valve Map File (Hammer Backup)", false, true)
-            };
+        new("rmx", "Worldcraft RMF (Hammer Backup)", false, true),
+        new("max", "Quake MAP Format (Hammer Backup)", false, true),
+        new("vmx", "Valve Map Format (Hammer Backup)", false, true),
+    ];
+
+    public static NativeFileDialog AddSupportedFiltersSave(this NativeFileDialog diag) {
+        foreach (var ext in SupportedExtensions
+                     .Where(x => x.CanSave)) {
+            diag.AddFilter(ext.Description, ext.Extension);
         }
+        return diag;
+    }
 
-        public static NativeFileDialog AddSupportedFiltersSave(this NativeFileDialog diag) {
-            foreach (var ext in GetSupportedExtensions()) {
-                if (ext.CanSave) {
-                    diag.AddFilter(ext.Description, ext.Extension[1..]);
-                }
-            }
-            return diag;
+    public static NativeFileDialog AddSupportedFiltersLoad(this NativeFileDialog diag) {
+        diag.AddFilter("All supported formats",
+            string.Join(',', SupportedExtensions
+                .Where(x => x.CanLoad)
+                .Select(x => x.Extension)));
+        foreach (var ext in SupportedExtensions
+                     .Where(x => x.CanLoad)) {
+            diag.AddFilter(ext.Description, ext.Extension);
         }
-
-        public static NativeFileDialog AddSupportedFiltersLoad(this NativeFileDialog diag) {
-            foreach (var ext in GetSupportedExtensions()) {
-                if (ext.CanLoad) {
-                    diag.AddFilter(ext.Description, ext.Extension[1..]);
-                }
-            }
-            return diag;
-        }
-
-        private static string ExecutableLocation()
-        {
-            return Assembly.GetEntryAssembly().Location;
-        }
+        return diag;
     }
 }
